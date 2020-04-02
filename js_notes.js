4a064e9fcb4a441a0834382efcc6e7834b0d5f0c
Mohamed Abdullah <mohamedfarid648@gmail.com>
2020-04-02 23:36:34 +0200

    Diff between 2 dates

--- js_notes.js
+++ js_notes.js
@@ -28,3 +28,20 @@ heroku apps:create myapp-name
 
 https://datatables.net/
 
+
+
+7. Diff between 2 dates
+let dt1 = new Date("October 13, 2014 08:11:00");
+let dt2 = new Date("October 13, 2014 11:13:00");
+var msec =dt2-dt1;
+    var mins = Math.floor(msec / 60000);
+
+    var hrs = Math.floor(mins / 60);
+
+    var days = Math.floor(hrs / 24);
+
+    var yrs = Math.floor(days / 365);
+ mins = mins % 60;
+hrs = hrs % 24;
+days = days % 365;
+
